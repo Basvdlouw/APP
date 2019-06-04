789eb9e08bb9a237bf116d7acf5014dcc8f3a67c
Bas van de Louw <basvdlouw@gmail.com>
2019-06-04 18:36:03 +0200

    adds faculty and sum functions

---
+++ src/main/java/han/ica/asd/app/recursion/FacultyCalculator.java
@@ -0,0 +1,39 @@
+package han.ica.asd.app.recursion;
+
+class FacultyCalculator {
+
+
+    /**
+     * Main method used to test the factorial methods.
+     * @param args
+     */
+    public static void main(String[] args) {
+        final int number = 5;
+        System.out.println("Recursive factorial of: " + number + " is " + calculateFactorialRecursive(number));
+        System.out.println("Non recursive factorial of: " + number + " is " + calculateFactorial(number));
+    }
+
+    /**
+     * This method calculates the factorial of the given number and returns it
+     * @param number of the factorial to be returned
+     * @return factorial of number
+     */
+    public static int calculateFactorial(int number) {
+        final int numberLength = number;
+        int result = 1;
+        for(int i = 0; i<numberLength; i++) {
+            result*=number;
+            number--;
+        }
+        return result;
+    }
+
+    /**
+     * This method calculates the factorial of the given number and returns it using recursive behaviour
+     * @param number of the factorial to be returned
+     * @return factorial of number
+     */
+    public static int calculateFactorialRecursive(int number) {
+        return number-1 == 0 ? number : number * calculateFactorialRecursive(number-1);
+    }
+}
---
+++ src/main/java/han/ica/asd/app/recursion/SumCalculator.java
@@ -0,0 +1,38 @@
+package han.ica.asd.app.recursion;
+
+public class SumCalculator {
+
+    /**
+     * Main method used to test the sum methods.
+     * @param args
+     */
+    public static void main(String[] args) {
+        final int number = 5;
+        System.out.println("Recursive sum of: " + number + " is " + calculateSumRecursive(number));
+        System.out.println("Non recursive sum of: " + number + " is " + calculateSum(number));
+    }
+
+    /**
+     * Calculates the "sum" of a given number using recursion, for example input 3 would result in 3 + 2 + 1 = 6.
+     * @param number
+     * @return sum of number
+     */
+    public static int calculateSumRecursive(int number) {
+       return number-1 == 0 ? number : number + calculateSumRecursive(number-1);
+    }
+
+    /**
+     * Calculates the "sum" of a given number, for exmaple input 3 would result in 3 + 2 + 1 = 6.
+     * @param number
+     * @return
+     */
+    public static int calculateSum(int number) {
+        final int numberLength = number;
+        int result = 0;
+        for(int i = 0; i<numberLength; i++) {
+            result += number;
+            number--;
+        }
+        return result;
+    }
+}
